1fbecca272efe67602498863d88f690c14291325
anstssiiagsv-spec <anstssiia.gsv@gmail.com>
2026-09-09 11:51:34 +0500

    Add solution

---
+++ 04-longest-substring-without-repeating-characters/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int left = 0;
+        int maxLength = 0;
+        unordered_set<char> seen;
+        for (int right = 0; right < s.size(); right++)
+        {
+            while (seen.find(s[right]) != seen.end())
+            {
+                seen.erase(s[left]);
+                left++;
+            }
+            seen.insert(s[right]);
+            maxLength = max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+};
